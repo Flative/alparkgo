a5519ac390b2b9bec604e5a51b33ac2d25729d48
ffaass <rlatnghd95@gmail.com>
2017-01-16 23:17:39 +0900

    Fix private method notation

--- alparkgo.py
+++ alparkgo.py
@@ -26,20 +26,20 @@ class Alparkgo(object):
 
     def run(self):
         slack_response = self.slack.rtm.start().body
-        self._parse_slack_information(slack_response)
+        self.__parse_slack_information(slack_response)
 
         websocket_url = slack_response['url']
         ws = websocket.WebSocketApp(websocket_url,
-                                    on_message=self._on_message(),
-                                    on_error=self._on_error(),
-                                    on_close=self._on_close())
-        ws.on_open = self._on_open()
+                                    on_message=self.__on_message(),
+                                    on_error=self.__on_error(),
+                                    on_close=self.__on_close())
+        ws.on_open = self.__on_open()
         ws.run_forever()
 
     def post_message(self, channel, message):
         self.slack.chat.post_message(channel, message, as_user=True)
 
-    def _parse_slack_information(self, slack_response):
+    def __parse_slack_information(self, slack_response):
         self.bot_user_id = slack_response['self']['id']
         self.bot_mention_string = ''.join(['@', self.bot_user_id])
         self.usernames = {user_info['id']: user_info['name'] for user_info in slack_response['users']}
@@ -50,7 +50,7 @@ class Alparkgo(object):
                 self.receiver_channel_id = self.dm_channels[user_id]
                 break
 
-    def _on_error(self):
+    def __on_error(self):
         def on_error(ws, error):
             self.slack.chat.post_message(settings.RECEIVER,
                                          'Alparkgo가 아파합니다.\n{error}'.format(error=error),
@@ -58,12 +58,12 @@ class Alparkgo(object):
 
         return on_error
 
-    def _on_close(self):
+    def __on_close(self):
         def on_close(ws):
             self.slack.chat.post_message(settings.RECEIVER, 'Alparkgo가 꺼졌습니다.', as_user=True)
         return on_close
 
-    def _on_open(self):
+    def __on_open(self):
         def on_open(ws):
             self.slack.chat.post_message(settings.RECEIVER,
                                          'Alparkgo가 켜졌습니다. '
@@ -71,21 +71,21 @@ class Alparkgo(object):
                                          as_user=True)
         return on_open
 
-    def _on_message(self):
+    def __on_message(self):
         def on_message(ws, ws_message):
             slack_message = json.loads(ws_message)
             if slack_message.get('type') == 'message':
-                self._receive_message(ws, slack_message)
+                self.__receive_message(ws, slack_message)
         return on_message
 
-    def _receive_message(self, ws, slack_message):
-        if self._is_not_alparkgo(slack_message['user']):
-            if self._is_message_from_receiver(slack_message):
-                self._response(ws, slack_message)
-            elif self._is_message_to_alparkgo(slack_message):
-                self._redirect_message_to_receiver(ws, slack_message)
+    def __receive_message(self, ws, slack_message):
+        if self.__is_not_alparkgo(slack_message['user']):
+            if self.__is_message_from_receiver(slack_message):
+                self.__response(ws, slack_message)
+            elif self.__is_message_to_alparkgo(slack_message):
+                self.__redirect_message_to_receiver(ws, slack_message)
 
-    def _response(self, ws, slack_message):
+    def __response(self, ws, slack_message):
         if not self.messages_queue.empty():
             message = self.messages_queue.get()
             response = {
@@ -98,7 +98,7 @@ class Alparkgo(object):
             }
             ws.send(json.dumps(response))
 
-    def _redirect_message_to_receiver(self, ws, slack_message):
+    def __redirect_message_to_receiver(self, ws, slack_message):
         message = Message(slack_message['text'],
                           slack_message['channel'],
                           slack_message['user'])
@@ -110,13 +110,13 @@ class Alparkgo(object):
         }
         ws.send(json.dumps(alert_to_receiver))
 
-    def _is_not_alparkgo(self, user_id):
+    def __is_not_alparkgo(self, user_id):
         return user_id != self.bot_user_id
 
-    def _is_message_from_receiver(self, slack_message):
+    def __is_message_from_receiver(self, slack_message):
         return slack_message['channel'] == self.receiver_channel_id
 
-    def _is_message_to_alparkgo(self, slack_message):
+    def __is_message_to_alparkgo(self, slack_message):
         return slack_message['channel'] in self.dm_channels.values() \
                     or self.bot_mention_string in slack_message['text']
 
